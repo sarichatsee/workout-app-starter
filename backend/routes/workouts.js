e7e30b1850f61a76e59b38a7756dea6b413c727a
fann <22034013@myrp.edu.sg>
2025-01-27 20:29:36 +0800

    updated home, workoutForm & workoutDetails with useAuthContext

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -10,6 +10,7 @@ const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
 
+// require auth for all workout routes
 router.use(requireAuth)
 
 // GET all workouts
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect }from 'react'
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
@@ -7,11 +8,15 @@ import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
   const {workouts, dispatch} = useWorkoutsContext()
+  const {user} = useAuthContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/api/workouts`
+      const response = await fetch('/api/workouts', {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      }
       )
       const json = await response.json()
 
@@ -20,8 +25,11 @@ const Home = () => {
       }
     }
 
-    fetchWorkouts()
-  }, [dispatch])
+    if (user) {
+      fetchWorkouts()
+    }
+    
+  }, [dispatch, user])
 
   return (
     <div className="home">
