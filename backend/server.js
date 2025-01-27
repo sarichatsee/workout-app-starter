a948ad2eeef15bba4e44912f2f9c45093b16238e
fann <22034013@myrp.edu.sg>
2025-01-27 16:00:15 +0800

    init, added env in frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,38 @@
-// what is going on
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const cors = require('cors');
 
 // express app
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
+
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-// routes
-app.use('/api/workouts', workoutRoutes)
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Workout API!');
+});
+
+// API routes
+app.use('/api/workouts', workoutRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
